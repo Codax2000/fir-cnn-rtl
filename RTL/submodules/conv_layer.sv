77e382169aec46ed5bec5a4530cf6e796ee2a33c
Alexander Knowlton <aknowlti2000@gmail.com>
2023-04-08 02:42:01 -0700

    Updated Convolutional Layer
    Changed to have more similarity to fully-connected layer, added serial input, simplified control signals. Now has serial-parallel interface.

--- RTL/submodules/conv_layer.sv
+++ RTL/submodules/conv_layer.sv
@@ -12,105 +12,116 @@ module conv_layer #(
 
     parameter INPUT_LAYER_HEIGHT=4,
     parameter KERNEL_HEIGHT=3,
-    parameter KERNEL_WIDTH=2,
+    parameter KERNEL_WIDTH=2, // 2 if using i and q, 1 if using only 1 channel
     parameter WORD_SIZE=16,
+    parameter INT_BITS=4, // integer bits in fixed-point arithmetic (default Q4.8)
     parameter LAYER_NUMBER=1,
     parameter CONVOLUTION_NUMBER=0) (
     
     input logic clk_i,
     input logic reset_i,
     
-    // helpful input interface
-    input logic valid_i,
-    output logic ready_o,
-    input logic [INPUT_LAYER_HEIGHT-1:0][KERNEL_WIDTH-1:0][WORD_SIZE-1:0] data_i,
+    // input interface
+    input logic start_i,
+    input logic [WORD_SIZE-1:0] data_i,
     
     // helpful output interface
     output logic valid_o,
     input logic yumi_i,
     output logic [INPUT_LAYER_HEIGHT - KERNEL_HEIGHT:0][WORD_SIZE-1:0] data_o);
     
-    // control logic variables
-    localparam num_iterations = KERNEL_HEIGHT * KERNEL_WIDTH;
+    parameter NUM_ITERATIONS = KERNEL_HEIGHT * KERNEL_WIDTH;
 
-    logic [$clog2(num_iterations)-1:0] rd_addr, rd_addr_lo; // used as memory address
-    logic [WORD_SIZE-1:0] mem_lo;
+    // control logic
+    enum logic [1:0] {eREADY=2'b00, eBUSY=2'b01, eDONE=2'b10} ps, ns;
 
-    // control signal to send to neurons
-    logic add_bias;
-    assign add_bias = rd_addr_lo == num_iterations;
+    logic [$clog2(NUM_ITERATIONS+1)-1:0] mem_addr;
+    logic add_bias, sum_en;
 
-    // FSM control logic
-    enum logic {eDONE=1'b0, eBUSY=1'b1} ps, ns; // present state, next state
+    logic [KERNEL_HEIGHT*KERNEL_WIDTH-1:0][WORD_SIZE-1:0] data_shift_reg;
+    logic signed [WORD_SIZE-1:0] mem_out;
 
+    assign sum_en = ps == eBUSY;
+    always_ff @(posedge clk_i)
+        add_bias <= mem_addr == NUM_ITERATIONS;
+
+    // next state logic
     always_comb begin
         case (ps)
+            eREADY:
+                if (start_i)
+                    ns = eBUSY;
+                else
+                    ns = eREADY;
             eBUSY:
                 if (add_bias)
                     ns = eDONE;
                 else
                     ns = eBUSY;
+            2'b11, // should never happen but good to have just in case, especially to avoid inferring a latch
             eDONE:
-                if (start_i)
-                    ns = eBUSY;
+                if (valid_o && yumi_i) // if handshake happens, be ready to start again
+                    ns = eREADY;
                 else
                     ns = eDONE;
         endcase
     end
 
-    always_ff @(posedge clk_i) begin
-        if (reset_i)
-            ps <= eDONE;
-        else
-            ps <= ns;
-    end
-    // end FSM control logic
-
+    // counter for memory address
     up_counter #(
-        .WORD_SIZE($clog2(num_iterations)),
-        .INPUT_MAX(num_iterations)
+        .WORD_SIZE($clog2(NUM_ITERATIONS+1)),
+        .INPUT_MAX(NUM_ITERATIONS)
     ) mem_address_counter (
-        .reset_i,
-        .clk_i,
         .start_i,
-        .data_o(rd_addr)
+        .clk_i,
+        .reset_i,
+
+        .data_o(mem_addr)
     );
 
-    always_ff @(posedge clk_i) // delay read address so that memory and address reach neurons at the same time
-        rd_addr_lo <= rd_addr;
+    // ROM for kernel values
+    ROM #(
+        .depth($clog2(NUM_ITERATIONS+1)),
+        .width(WORD_SIZE),
+        .neuron_type(0),
+        .layer_number(LAYER_NUMBER),
+        .neuron_number(CONVOLUTION_NUMBER)
+    ) weight_bias_mem (
+        .clk_i,
+        .addr_i(mem_addr),
+        .data_o(mem_out)
+    );
 
-    ROM #(.depth($clog2(num_iterations)),
-          .width(WORD_SIZE),
-          .neuron_type(0),
-          .layer_number(LAYER_NUMBER),
-          .neuron_number(CONVOLUTION_NUMBER) // always 0 for convolutional
-    ) conv_layer_mem (
-          .clk_i,
-          .addr_i(rd_addr),
-          .data_o(mem_lo)
-          );
+    // shift register for holding inputs
+    shift_register #(
+        .WORD_SIZE(WORD_SIZE),
+        .REGISTER_LENGTH(KERNEL_HEIGHT * KERNEL_WIDTH)
+    ) input_shift_reg (
+        .data_i,
+        .shift_en_i(1'b1), // not currently used
+        .clk_i,
+        .reset_i,
+        .data_o(data_shift_reg)
+    );
 
+    // generate 'neurons' (really just logical units from fully-connected layer)
     genvar i;
     generate
         for (i = 0; i < INPUT_LAYER_HEIGHT - KERNEL_HEIGHT + 1; i = i + 1) begin
-            conv_node #(
+            logical_unit #(
                 .WORD_SIZE(WORD_SIZE),
-                .KERNEL_HEIGHT(KERNEL_HEIGHT),
-                .KERNEL_WIDTH(KERNEL_WIDTH)
-            ) node (
+                .INT_BITS(INT_BITS)
+            ) LU (
+                .mem_i(mem_out),
+                .data_i(data_shift_reg[i*KERNEL_WIDTH]), // allow for multiple kernel widths
+                .add_bias,
+                .sum_en,
                 .clk_i,
                 .reset_i,
-                .start_i,
-                .ps,
-                .data_i(data_i[INPUT_LAYER_HEIGHT - KERNEL_HEIGHT + 1 + i:i]),
-                .weight_i(mem_lo),
-                .input_index(rd_addr_lo),
-                .add_bias,
                 .data_o(data_o[i])
             );
         end
     endgenerate
 
-    // output done signal after add bias is finished
-    assign done_o = ps == eDONE;
+
 endmodule
