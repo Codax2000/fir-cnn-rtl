0c568932eee03334af0d8824922ac52de36aeb5a
Alexander Knowlton <aknowlti2000@gmail.com>
2023-04-10 16:07:24 -0700

    Added Gantt chart

--- Scripts/python/gantt_chart.py
+++ Scripts/python/gantt_chart.py
@@ -2,16 +2,24 @@ import pandas as pd
 import altair as alt
 
 def get_data():
-    data = pd.read_excel("../data/ECE478_526 Status.xlsx", 1, parse_dates=['Start Date', 'End Date'])
+    start_date = pd.to_datetime('3/27/2023')
+    data = pd.read_excel("./Scripts/data/ECE478_526 Status.xlsx", 1, parse_dates=['Start Date', 'End Date'])
     print(data.info())
     return data
 
 def gantt_chart(data):
-    alt.Chart(data).mark_bar().encode(
-        x='Start Date:Q',
-        x2='End Date:Q',
-        y='Task:N'
-    ).save('../data/gantt_chart.html')
+    alt.Chart(data, title='Neural Net Gantt Chart').mark_bar().encode(
+        x='Start Date:T',
+        x2='End Date:T',
+        y=alt.Y('Task:N', sort='x'),
+        color=alt.Color('Assignee:N',
+                        scale=alt.Scale(
+                            domain=['Alex', 'Eugene', 'Alex, Eugene'],
+                            range=['blue', 'red', 'green']
+                        ))
+    ).properties(
+
+    ).save('./Scripts/data/gantt_chart.html')
 
 def main():
     data = get_data()
